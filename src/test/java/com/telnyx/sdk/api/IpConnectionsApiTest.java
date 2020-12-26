48b4d26d4d9e17298d88c733f6682cd8a7db4ac9
Hossein Shemshadi <hossein.shemshadi@sinansoft.com>
2020-12-26 09:36:41 +0330

    Implement init commit for IpConnectionsApi test

--- src/test/java/com/telnyx/sdk/api/IpConnectionsApiTest.java
+++ src/test/java/com/telnyx/sdk/api/IpConnectionsApiTest.java
@@ -13,21 +13,24 @@
 
 package com.telnyx.sdk.api;
 
-import com.telnyx.sdk.*;
-import com.telnyx.sdk.auth.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.telnyx.sdk.ApiClient;
+import com.telnyx.sdk.ApiException;
+import com.telnyx.sdk.Configuration;
+import com.telnyx.sdk.JSON;
+import com.telnyx.sdk.auth.HttpBearerAuth;
+import com.telnyx.sdk.model.AnchorsiteOverride;
+import com.telnyx.sdk.model.ConnectionRtcpSettings;
+import com.telnyx.sdk.model.CreateInboundIpRequest;
 import com.telnyx.sdk.model.CreateIpConnectionRequest;
+import com.telnyx.sdk.model.DtmfType;
+import com.telnyx.sdk.model.EncryptedMedia;
 import com.telnyx.sdk.model.IpConnectionResponse;
-import com.telnyx.sdk.model.ListIpConnectionsResponse;
-import com.telnyx.sdk.model.UpdateIpConnectionRequest;
-import org.junit.Assert;
-import org.junit.Ignore;
+import com.telnyx.sdk.model.OutboundIp;
+import org.junit.Before;
 import org.junit.Test;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Collections;
 
 /**
  * API tests for IpConnectionsApi
@@ -35,29 +38,88 @@ import java.util.Map;
 public class IpConnectionsApiTest {
 
     private final IpConnectionsApi api = new IpConnectionsApi();
+    private ObjectMapper mapper;
+
+    @Before
+    public void setup() {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath(TestConfiguration.MOCK_SERVER_URL);
+
+        HttpBearerAuth bearerAuth = (HttpBearerAuth) defaultClient.getAuthentication("bearerAuth");
+        bearerAuth.setBearerToken(TestConfiguration.API_KEY);
+
+        mapper = new JSON().getMapper();
+    }
 
     /**
      * Create an Ip connection
-     *
+     * <p>
      * Creates an IP connection.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
-    public void createIpConnectionTest() throws ApiException {
-        //CreateIpConnectionRequest createIpConnectionRequest = null;
-        //IpConnectionResponse response = api.createIpConnection(createIpConnectionRequest);
+    public void createIpConnection_whenDefaultRequest_returnsIpConnection() throws ApiException {
+        CreateIpConnectionRequest createIpConnectionRequest = new CreateIpConnectionRequest()
+                .active(true)
+                .anchorsiteOverride(AnchorsiteOverride.CHICAGO_IL)
+                .connectionName("some_connection")
+                .defaultOnHoldComfortNoiseEnabled(true)
+                .dtmfType(DtmfType.RFC_2833)
+                .encodeContactHeaderEnabled(false)
+                .encryptedMedia(EncryptedMedia.SRTP)
+                .inbound(new CreateInboundIpRequest()
+                        .aniNumberFormat(CreateInboundIpRequest.AniNumberFormatEnum._E_164)
+                        .channelLimit(10)
+                        .codecs(Collections.singletonList("G722"))
+                        .defaultRoutingMethod(CreateInboundIpRequest.DefaultRoutingMethodEnum.SEQUENTIAL)
+                        .dnisNumberFormat(CreateInboundIpRequest.DnisNumberFormatEnum._E164)
+                        .generateRingbackTone(true)
+                        .isupHeadersEnabled(true)
+                        .prackEnabled(true)
+                        .privacyZoneEnabled(true)
+                        .sipCompactHeadersEnabled(true)
+                        .sipRegion(CreateInboundIpRequest.SipRegionEnum.US)
+                        .sipSubdomain("test")
+                        .sipSubdomainReceiveSettings(CreateInboundIpRequest.SipSubdomainReceiveSettingsEnum.ONLY_MY_CONNECTIONS)
+                        .timeout1xxSecs(10)
+                        .timeout2xxSecs(20)
+                )
+                .onnetT38PassthroughEnabled(false)
+                .outbound(new OutboundIp()
+                        .aniOverride("test")
+                        .aniOverrideType(OutboundIp.AniOverrideTypeEnum.ALWAYS)
+                        .callParkingEnabled(true)
+                        .channelLimit(10)
+                        .generateRingbackTone(true)
+                        .instantRingbackEnabled(true)
+                        .ipAuthenticationMethod(OutboundIp.IpAuthenticationMethodEnum.TOKEN)
+                        .ipAuthenticationToken("test")
+                        .localization("test")
+                        .outboundVoiceProfileId("123")
+                        .t38ReinviteSource(OutboundIp.T38ReinviteSourceEnum.TELNYX)
+                        .techPrefix("test")
+                )
+                .rtcpSettings(new ConnectionRtcpSettings()
+                        .captureEnabled(true)
+                        .port(ConnectionRtcpSettings.PortEnum.RTCP_MUX)
+                        .reportFrequencySecs(10)
+                )
+                .transportProtocol(CreateIpConnectionRequest.TransportProtocolEnum.UDP);
+        //TODO: missed webhook_event_failover_url
+        //TODO: missed webhook_event_url
+        //TODO: missed webhook_timeout_secs
+
+        IpConnectionResponse response = api.createIpConnection(createIpConnectionRequest);
         // TODO: test validations
     }
 
     /**
      * Delete an Ip connection
-     *
+     * <p>
      * Deletes an existing IP connection.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
     public void deleteIpConnectionTest() throws ApiException {
@@ -68,11 +130,10 @@ public class IpConnectionsApiTest {
 
     /**
      * List Ip connections
-     *
+     * <p>
      * Returns a list of your IP connections.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
     public void listIpConnectionsTest() throws ApiException {
@@ -81,23 +142,16 @@ public class IpConnectionsApiTest {
         //String filterConnectionNameContains = null;
         //String filterOutboundOutboundVoiceProfileId = null;
         //String sort = null;
-        //ListIpConnectionsResponse response = api.listIpConnections()
-        //        .pageNumber(pageNumber)
-        //        .pageSize(pageSize)
-        //        .filterConnectionNameContains(filterConnectionNameContains)
-        //        .filterOutboundOutboundVoiceProfileId(filterOutboundOutboundVoiceProfileId)
-        //        .sort(sort)
-        //        .execute();
+        //ListIpConnectionsResponse response = api.listIpConnections(pageNumber, pageSize, filterConnectionNameContains, filterOutboundOutboundVoiceProfileId, sort);
         // TODO: test validations
     }
 
     /**
      * Retrieve an Ip connection
-     *
+     * <p>
      * Retrieves the details of an existing ip connection.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
     public void retrieveIpConnectionTest() throws ApiException {
@@ -108,11 +162,10 @@ public class IpConnectionsApiTest {
 
     /**
      * Update an Ip connection
-     *
+     * <p>
      * Updates settings of an existing IP connection.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
     public void updateIpConnectionTest() throws ApiException {
