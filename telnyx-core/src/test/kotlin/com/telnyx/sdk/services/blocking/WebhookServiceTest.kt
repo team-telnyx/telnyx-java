ff6a0f54e174526ea19b9bfea260868404baa529
Nick Bair <8889403+nicholasbair@users.noreply.github.com>
2026-02-24 14:05:50 -0700

    fix: linting

--- telnyx-core/src/test/kotlin/com/telnyx/sdk/services/blocking/WebhookServiceTest.kt
+++ telnyx-core/src/test/kotlin/com/telnyx/sdk/services/blocking/WebhookServiceTest.kt
@@ -1,6 +1,7 @@
 // File generated from our OpenAPI spec by Stainless.
 
 package com.telnyx.sdk.services.blocking
+
 import com.telnyx.sdk.TestServerExtension
 import com.telnyx.sdk.client.okhttp.TelnyxOkHttpClient
 import com.telnyx.sdk.core.UnwrapWebhookParams
